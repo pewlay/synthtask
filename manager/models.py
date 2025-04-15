1c20134ec0cd630e6ed6fd7c7b45f68007c670fa
Polina Haltseva <polinayut@gmail.com>
2025-04-15 22:57:02 +0300

    Update models: add Meta.ordering and __str__ methods

--- manager/models.py
+++ manager/models.py
@@ -6,13 +6,31 @@ from django.db import models
 class Position(models.Model):
     name = models.CharField(unique=True, max_length=255)
 
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return self.name
+
 
 class Worker(AbstractUser):
     position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name="workers")
 
+    class Meta:
+        ordering = ("first_name", "last_name", )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} Position: {self.position}"
+
 
 class TaskType(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
@@ -27,9 +45,17 @@ class Task(models.Model):
     deadline = models.DateTimeField()
     is_completed = models.BooleanField(default=False)
     priority = models.CharField(
-        max_length=9,
+        max_length=10,
         choices=Priority.choices,
         default=Priority.LOW,
     )
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
     assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")
+
+    class Meta:
+        ordering = ("priority", "-deadline", "name")
+
+    def __str__(self):
+        return (f"{self.name} Priority: {self.priority} \n"
+                f"Deadline: {self.deadline.strftime('%Y-%m-%d %H:%M')} \n"
+                f"{'Done' if self.is_completed else 'Not finished'}")
