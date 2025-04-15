53456ed0a674180b0f87f5cc470d0c4124a91a65
Polina Haltseva <polinayut@gmail.com>
2025-04-15 22:03:23 +0300

    Add auth_user_model to settings.py

--- SynthTask/settings.py
+++ SynthTask/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'manager'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'manager.Worker'
