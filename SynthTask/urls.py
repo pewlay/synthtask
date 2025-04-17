f84ff015b6113b78def12639fb18bbd22ccc92d5
Polina Haltseva <polinayut@gmail.com>
2025-04-17 15:16:29 +0300

    Add new url

--- SynthTask/urls.py
+++ SynthTask/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('manager/', include('manager.urls', namespace='manager'))
-]
+    path('manager/', include('manager.urls', namespace='manager')),
+    path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
