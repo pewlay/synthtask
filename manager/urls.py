57988ba9c8f0859319126490671f7c36e296b059
Polina Haltseva <polinayut@gmail.com>
2025-04-17 14:50:06 +0300

    Update urls in urls.py

--- manager/urls.py
+++ manager/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
-from manager.views import home
+from manager.views import (
+    home,
+    PositionListView,
+    TaskTypeListView,
+    WorkerListView,
+    WorkerDetailView,
+    TaskListView,
+    TaskDetailView,
+)
 
 
 app_name = 'manager'
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('positions/', PositionListView.as_view(), name='position-list'),
+    path('types/', TaskTypeListView.as_view(), name='task_type-list'),
+    path('workers/', WorkerListView.as_view(), name='worker-list'),
+    path('workers/<int:pk>/', WorkerDetailView.as_view(), name='worker-detail'),
+    path('tasks/', TaskListView.as_view(), name='task-list'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
 ]
 
