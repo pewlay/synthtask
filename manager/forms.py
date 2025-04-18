e47f53d2fcd2b95f49a45647b32326357b5fac34
Polina Haltseva <polinayut@gmail.com>
2025-04-18 11:26:47 +0300

    Add forms

---
+++ manager/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from django.forms.widgets import DateInput
+
+from manager.models import Worker, Task, Position, TaskType
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class PositionForm(forms.ModelForm):
+    class Meta:
+        model = Position
+        fields = '__all__'
+
+
+class TaskTypeForm(forms.ModelForm):
+    class Meta:
+        model = TaskType
+        fields = '__all__'
+
+
+class WorkerForm(forms.ModelForm):
+    class Meta:
+        model = Worker
+        fields = ['first_name', 'last_name', 'email', 'position', 'phone_number', 'is_active']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'position': forms.Select(attrs={'class': 'form-control'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'is_active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'priority', 'deadline', 'assignees', 'task_type']
+        widgets = {
+            'deadline': DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        }
+
+
+class UserCreateForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
